472bc4e0fc8a074d2fda460317e26bd75f6c91d9
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-10-14 23:15:28 +0600

    User Login

--- App_Login/views.py
+++ App_Login/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from .forms import *
+from .models import *
 
 
 def sign_up(request):
@@ -13,6 +15,27 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             registered = True
-            pass
+            user_profile = UserProfile(user=user)
+            return HttpResponseRedirect(reverse('App_Login:login'))
 
     return render(request, 'App_Login/signup.html', context={'title': 'Sign Up', 'form': form})
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('App_Posts:home'))
+
+    return render(request, 'App_Login/login.html', context={'title': 'Login', 'form': form})
+
+
+@login_required
+def edit_profile(request):
+    pass
